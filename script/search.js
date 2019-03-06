69800c480c5552b65755f926ae7bf2b7f59a64af
James Wu <527143499@qq.com>
2019-03-06 13:27:58 +0800

    Update search.js
    页码链接问题

--- script/search.js
+++ script/search.js
@@ -12,7 +12,7 @@ var pageName = '搜索结果';
 var keyWord = urlPart.search;
 
 //每页数量
-var pageNum = 16;
+var pageNum = 12;
 //当前页数
 var pageNow = parseInt(urlPart.page ? urlPart.page : 1);
 
@@ -27,19 +27,14 @@ $(document).ready(function() {
 
 			//无搜索结果
 			if (arrayData['babyAll'].length == 0)
-				$('#babyAll').append("<div id='noResult'>无搜索结果</div>");
+				$('#babyAll').append("<div style='margin-top: 10px; margin-left: 15px;'>无搜索结果</div>");
 
-			//把板块数据几个一行分组
-			var eachRowNum = 2;
-			if (window.innerWidth > 1000)
-				eachRowNum = 4;
-			else if (window.innerWidth > 700)
-				eachRowNum = 3;
-
+			//把板块数据每4个一行分组
 			var babyAll = new Array();
-			var babyRealSum = arrayData['babyAll'].length;  //真实传来的宝贝个数
-			for (var i=0; i+eachRowNum<=21 && i<=babyRealSum; i+=eachRowNum)
-				babyAll.push(arrayData['babyAll'].slice(i, i+eachRowNum));
+			var i = 0;
+			for (; i<(arrayData['babyAll'].length/4)-1; i++)
+				babyAll.push(arrayData['babyAll'].slice(i*4, i*4+4));
+			babyAll.push(arrayData['babyAll'].slice(i*4, arrayData['babyAll'].length));
 
 			//页码页数处理
 			pageSum = Math.ceil(arrayData['babySum'] / pageNum);
@@ -49,7 +44,7 @@ $(document).ready(function() {
 			
 			//数据更新
 			new Vue({
-				el: '#mainBox',
+				el: '#rightBox',
 				data: {
 					babyAllRows: babyAll,
 					keyWord: decodeURI(keyWord),
@@ -59,67 +54,34 @@ $(document).ready(function() {
 				},
 				mounted() {
 					rightBind();  //same.js中的绑定事件
-					rightDellWith();  //页码处理
-					(function() {  //babyBox最后一行个数不满
-						var lastRowNum = $('.row').eq(-1).children().length;
-						var lastEachWidth = $('.row').eq(-1).children().outerWidth(true);
-						if (lastRowNum != eachRowNum)
-						{
-							for (var i=0; i<eachRowNum-lastRowNum; i++)
-								$('.row').eq(-1).append("<div style='width:{0}px;'></div>".format(lastEachWidth));
-						}
-					})();
+					rightDellWith();  //页码、排序等元素处理
 				}
 			});
+		},
+		error: function() {
+			console.log('Ajax data error.');
 		}
 	});
 
 });
 
-//页码处理
+//页码、排序等元素处理
 function rightDellWith()
 {
-	//当前页码
-	$('.number').eq(pageNow-1).removeClass('clickable');
-	$('.number').eq(pageNow-1).removeAttr('href');
-
-	//只有一页
+	//页码处理
 	if (pageSum <= 1)
 		$('#pageNumber').remove();
-	//页码太多
-	if (pageSum > 6)
-	{
-		if (pageNow <= 4)  //最左
-		{
-			for (var i=7; i<pageSum; i++)
-				$('a.number').eq(7).remove();
-			$('a.number').eq(-1).after('<span>...</span>');
-		}
-		else if (pageNow >= pageSum-3)  //最右
-		{
-			for (var i=0; i<pageSum-7; i++)
-				$('a.number').eq(0).remove();
-			$('.lastpage').eq(0).after('<span>...</span>');
-		}
-		else
-		{
-			for (var i=0; i<pageNow-4; i++)
-				$('a.number').eq(0).remove();
-			for (i=7; i<pageSum; i++)
-				$('a.number').eq(7).remove();
-			$('.lastpage').after('<span>...</span>');
-			$('a.number').eq(-1).after('<span>...</span>');
-		}
-	}
-
 	//上一页
 	if (pageNow == 1)
 		$('.lastpage').remove();
 	else
-		$('.lastpage').attr('href', 'babylist.html?type={0}&page={1}&sortWay={2}'.format(navigChosen, pageNow-1, pageSort));
+		$('.lastpage').attr('href', 'search.html?search={0}&page={1}'.format(keyWord, pageNow-1));
 	//下一页
 	if (pageNow == pageSum)
 		$('.nextpage').remove();
 	else
-		$('.nextpage').attr('href', 'babylist.html?type={0}&page={1}&sortWay={2}'.format(navigChosen, pageNow+1, pageSort));
+		$('.nextpage').attr('href', 'search.html?search={0}&page={1}'.format(keyWord, pageNow+1));
+	//当前页码
+	$('.number').eq(pageNow-1).removeClass('clickable');
+	$('.number').eq(pageNow-1).removeAttr('href');
 }
